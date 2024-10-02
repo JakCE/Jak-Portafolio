735a9a868046a4a9afd0f9e1ce67828da68e6540
jak.campos <practicas.0020@sesitdigital.com>
2024-10-02 18:26:19 -0500

    add section expirence y estilos

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { IconsModule } from '../../icons.module';
 import { NgIf } from '@angular/common';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
@@ -11,6 +11,46 @@ import { TypingAnimationComponent } from "../../components/typing-animation/typi
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
-  
+export class AboutComponent implements OnInit{
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.cursor-pointer a');
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const id = entry.target.getAttribute('id');
+          const link = document.querySelector(`a[routerLink][fragment="${id}"]`);
+          const span = link?.previousElementSibling as HTMLElement;
+
+          if (entry.isIntersecting) {
+            // Desactivar cualquier otro link antes de activar el nuevo
+            navLinks.forEach((navLink) => {
+              this.renderer.removeClass(navLink, 'text-orange-500');
+              const spanSibling = navLink.previousElementSibling as HTMLElement;
+              this.renderer.removeClass(spanSibling, 'border-orange-500');
+              this.renderer.removeClass(spanSibling, 'span-active');
+            });
+
+            // Activar solo el link de la sección visible
+            this.renderer.addClass(link, 'text-orange-500');
+            if (span) {
+              this.renderer.addClass(span, 'border-orange-500');
+              this.renderer.addClass(span, 'span-active'); // Añadir tamaño extra
+            }
+          }
+        });
+      },
+      {
+        rootMargin: '0px 0px 0px 0px',  // Márgenes de la vista para asegurar que detecta la sección activa
+        threshold: 0.5 // Detecta cuando el 50% de la sección es visible
+      }
+    );
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  }
 }
