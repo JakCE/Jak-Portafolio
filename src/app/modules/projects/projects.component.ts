2be4b58274246b74e1199b385678585aa889b353
jak.campos <practicas.0020@sesitdigital.com>
2024-10-06 13:55:26 -0500

    upp

--- src/app/modules/projects/projects.component.ts
+++ src/app/modules/projects/projects.component.ts
@@ -10,6 +10,24 @@ import { initFlowbite } from 'flowbite';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent implements OnInit{
+  arrayDataProjects: any = [
+    {
+      imgSrc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg',
+      title: 'Calculadora',
+      description: 'Calculadora interactiva desarrollada con funcionalidades básicas y avanzadas, diseñada para realizar operaciones matemáticas de manera eficiente y fácil de usar.',
+      linkDemo: '#',
+      linkRepo: '#',
+      badgets: ['Angular', 'Tailwind', 'Flowbite UI'],
+    },
+    {
+      imgSrc: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg',
+      title: 'Portafolio',
+      description: 'Portafolio moderno con contenido de calidad.',
+      linkDemo: '#',
+      linkRepo: '#',
+      badgets: ['Angular', 'Tailwind', 'Flowbite UI'],
+    }
+  ]
   ngOnInit(): void {
     initFlowbite();
   }
