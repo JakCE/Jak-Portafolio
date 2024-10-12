12bac7c84cf1bf0f1825cfb1125648c7963a0295
jak.campos <practicas.0020@sesitdigital.com>
2024-10-11 21:41:43 -0500

    update projects section

--- src/app/modules/projects/projects.component.ts
+++ src/app/modules/projects/projects.component.ts
@@ -16,6 +16,7 @@ export class ProjectsComponent implements OnInit{
     {
       imgSrc: './projects_img/ft_portfolio.png',
       title: 'Portfolio',
+      state: 'Active',
       description: 'Web portfolio with responsive design, enhanced accessibility, and interactive components.',
       linkDemo: 'https://jak-portafolio.vercel.app/home',
       linkRepo: 'https://github.com/JakCE/Jak-Portafolio',
@@ -24,6 +25,7 @@ export class ProjectsComponent implements OnInit{
     {
       imgSrc: './projects_img/calc.png',
       title: 'Calculator',
+      state: 'Active',
       description: 'Simple web calculator with basic functions for addition, subtraction, multiplication, and division, designed with a clear and user-friendly interface.',
       linkDemo: 'https://calculator-sooty-beta.vercel.app/',
       linkRepo: 'https://github.com/JakCE/calculator',
@@ -32,6 +34,7 @@ export class ProjectsComponent implements OnInit{
     {
       imgSrc: './projects_img/progress_img.jpg',
       title: 'The Number One App',
+      state: 'In Progress',
       description: 'I\'m developing a full-stack web application to streamline the registration and payment tracking processes for \'The Number One\' academy. The system features a user-friendly front-end for easy registration and payment management, while the back-end ensures secure and efficient data handling and processing.',
       linkDemo: '#',
       linkRepo: '#',
